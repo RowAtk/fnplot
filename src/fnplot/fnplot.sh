e590460a9b4807ce2be3dc2be6370811f0680a80
Rowan Atkinson <rowaneatkinson@gmail.com>
2019-11-30 23:42:44 -0500

    cleaned up project

--- src/fnplot/fnplot.sh
+++
@@ -1,4 +0,0 @@
-jflex syntax/FnPlotLexer
-cup -parser FnPlotParser -destdir syntax syntax/FnPlotParser.cup
-javac -classpath ".:/usr/share/java/cup.jar" gui/*.java syntax/*.java semantics/*.java sys/*.java values/*.java
-java -classpath ".:/usr/share/java/cup.jar" gui/FnPlotFrame
--- src/fnplot/run.sh
+++
@@ -1,4 +0,0 @@
-jflex src/fnplot/syntax/FnPlotLexer
-cup -parser FnPlotParser -destdir syntax src/fnplot/syntax/FnPlotParser.cup
-javac -cp ".:/usr/share/java/cup.jar:cs34q.gfx.jar" src/fnplot/gui/*.java src/fnplot/syntax/*.java src/fnplot/semantics/*.java src/fnplot/sys/*.java src/fnplot/values/*.java
-# java -cp ".:/usr/share/java/cup.jar:cs34q.gfx.jar" src/fnplot/fnplot/gui/FnPlotFrame
